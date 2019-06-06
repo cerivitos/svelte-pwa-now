8e0c06382c25ac2676f3e0d9317b87c275a62ae5
CHANWAIKIT\ceriv <cerivitos@gmail.com>
2019-06-06 23:17:27 +0800

    Create Map component

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,16 @@
 <script>
   import Searchbar from "./components/Searchbar.svelte";
+  import Map from './components/Map.svelte';
 </script>
 
 <style lang="postcss">
 
 </style>
 
-<main class="p-4">
-  <Searchbar/>
+<main>
+  <!--For global margin-->
+  <div class="absolute p-4 w-full z-10">
+    <Searchbar/>  
+  </div>
+  <Map/>
 </main>
---
+++ src/components/Map.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { currentLat, currentLong } from "../store/store.js";
+  import { onMount } from "svelte";
+  import L from "leaflet";
+
+  let map;
+
+  onMount(async () => {
+    map = L.map("map").setView([1.29027, 103.851959], 12);
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+  });
+
+  $: if (map !== undefined) {
+    map.flyTo(new L.LatLng($currentLat, $currentLong), 18);
+  }
+</script>
+
+<div id="map" class="w-screen h-screen" style="z-index: -1;"></div>
