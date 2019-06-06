65477f0090337a9d79075a003e9db05a195c39b4
CHANWAIKIT\ceriv <cerivitos@gmail.com>
2019-06-06 23:18:08 +0800

    Remove searchLocation string

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,4 @@
 import { writable } from "svelte/store";
 
-export const searchLocation = writable("");
-export const currentLat = writable("");
-export const currentLong = writable("");
+export const currentLat = writable(1.29027);
+export const currentLong = writable(103.851959);
