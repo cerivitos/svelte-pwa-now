87c8129558347fe3bef699212d02ce1665ddfe12
CHANWAIKIT\ceriv <cerivitos@gmail.com>
2019-06-11 00:20:58 +0800

    Create information modal for place

--- src/App.svelte
+++ src/App.svelte
@@ -2,16 +2,18 @@
   import Searchbar from "./components/Searchbar.svelte";
   import Map from './components/Map.svelte';
   import PlaceList from './components/PlaceList.svelte';
+  import InfoModal from './components/InfoModal.svelte';
 </script>
 
 <style lang="postcss">
 
 </style>
 
-<main >
+<main class="overflow-hidden">
   <!--For global margin-->
   <div class="absolute p-4 w-full" style="z-index:1000" >
     <PlaceList/>
   </div>
   <Map/>
+  <InfoModal/>
 </main>
