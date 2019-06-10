4b65ea5c99deb075b90c0f3c47cd4431e6cbf857
CHANWAIKIT\ceriv <cerivitos@gmail.com>
2019-06-11 00:19:29 +0800

    Add custom heights

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        "80": "20rem",
+        "96": "24rem",
+        "112": "28rem",
+        "128": "32rem"
+      }
+    }
   },
   variants: {},
   plugins: []
-}
+};
