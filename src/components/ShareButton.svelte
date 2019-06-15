c03b792869b873346937faf239f22f7d0c6073b4
CHANWAIKIT\ceriv <cerivitos@gmail.com>
2019-06-15 17:52:44 +0800

    Change share button style

--- src/components/ShareButton.svelte
+++ src/components/ShareButton.svelte
@@ -69,7 +69,7 @@
 
 {#if !showWebShare}
 <button
-  class="w-32 rounded border border-blue-600 bg-gray-200 hover:bg-gray-300 hover:shadow text-blue-600 font-medium px-5 py-2 mr-2 inline-flex items-center"
+  class="w-32 rounded bg-gray-200 hover:bg-gray-300 hover:shadow text-blue-600 font-medium px-5 py-2 mr-2 inline-flex items-center"
   on:click="{handleShare}"
 >
   <svg
