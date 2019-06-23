1e91dd8f332f2bdb4db20ddaa9ad78e258e2b673
CHANWAIKIT\ceriv <cerivitos@gmail.com>
2019-06-23 17:06:42 +0800

    Change to round icon Add square apple icon

--- src/App.svelte
+++ src/App.svelte
@@ -23,7 +23,7 @@
   <meta name="apple-mobile-web-app-capable" content="yes">
   <meta name="apple-mobile-web-app-status-bar-style" content="black">
   <meta name="apple-mobile-web-app-title" content="sgtoilet">
-  <link rel="apple-touch-icon" href="">
+  <link rel="apple-touch-icon" href="/assets/apple-icon-512x512.png">
   <meta name="description" content="An unofficial listing of toilets rated by the Restroom Association (Singapore)">
   <meta name="theme-color" content="#5bd1d7" />
   <link href="https://fonts.googleapis.com/css?family=Oswald|Roboto&display=swap" rel="stylesheet">
