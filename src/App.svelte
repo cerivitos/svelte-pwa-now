89644be2e88a6b9aa736b15807869f0c0b1d825f
CHANWAIKIT\ceriv <cerivitos@gmail.com>
2019-06-23 21:01:15 +0800

    Move head info to index.html

--- src/App.svelte
+++ src/App.svelte
@@ -18,24 +18,6 @@
 
 <style lang="postcss"></style>
 
-<svelte:head>
-  <link rel="manifest" href="/manifest.json">
-  <link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png">
-  <link rel="mask-icon" href="/assets/safari-pinned-tab.svg" color="#5bbad5">
-  <link rel="shortcut icon" href="/assets/favicon.ico">
-  <meta name="msapplication-TileColor" content="#b91d47">
-  <meta name="msapplication-config" content="/assets/browserconfig.xml">
-  <meta name="apple-mobile-web-app-capable" content="yes">
-  <meta name="apple-mobile-web-app-status-bar-style" content="black">
-  <meta name="apple-mobile-web-app-title" content="sgtoilet">
-  <meta name="description" content="An unofficial listing of toilets rated by the Restroom Association (Singapore)">
-  <meta name="theme-color" content="#5bd1d7" />
-  <link href="https://fonts.googleapis.com/css?family=Oswald|Roboto&display=swap" rel="stylesheet">
-  <title>sgtoilet | Toilets in Singapore</title>
-</svelte:head>
-
   <main class="overflow-hidden">
     <Router/>
     <PlaceList />
