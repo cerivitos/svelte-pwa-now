1779f6559c7bfb9cae27ca9c2a45f5ff8c198f85
CHANWAIKIT\ceriv <cerivitos@gmail.com>
2019-06-24 00:01:32 +0800

    Update service worker

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -4,24 +4,58 @@ var filesToCache = [
   "/index.html",
   "/main.css",
   "/main.js",
-  "/components.css"
+  "/components.css",
+  "https://fonts.googleapis.com/css?family=Oswald|Roboto&display=swap"
 ];
 self.addEventListener("install", function(e) {
-  console.log("[ServiceWorker] Install");
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
-      console.log("[ServiceWorker] Caching app shell");
       return cache.addAll(filesToCache);
     })
   );
 });
-self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(thisCacheName) {
+          if (thisCacheName !== cacheName) {
+            console.log(
+              "[ServiceWorker] Removing Cached Files from Cache - ",
+              thisCacheName
+            );
+            return caches.delete(thisCacheName);
+          }
+        })
+      );
+    })
+  );
 });
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request, { ignoreSearch: true }).then(response => {
-      return response || fetch(event.request);
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      if (response) {
+        console.log("[ServiceWorker] Found in Cache", e.request.url, response);
+        return response;
+      }
+      var requestClone = e.request.clone();
+      fetch(requestClone)
+        .then(function(response) {
+          if (!response) {
+            console.log("[ServiceWorker] No response from fetch ");
+            return response;
+          }
+
+          var responseClone = response.clone();
+          caches.open(cacheName).then(function(cache) {
+            cache.put(e.request, responseClone);
+            console.log("[ServiceWorker] New Data Cached", e.request.url);
+            return response;
+          });
+        })
+        .catch(function(err) {
+          console.log("[ServiceWorker] Error Fetching & Caching New Data", err);
+        });
     })
   );
 });
