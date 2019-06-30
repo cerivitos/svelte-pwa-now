d9fe169d6bdcccaa66c8cb3ab566a2f7bd2c1e44
CHANWAIKIT\ceriv <cerivitos@gmail.com>
2019-06-30 15:47:41 +0200

    Change key back to prod

--- dist/main.js
+++ dist/main.js
@@ -1052,8 +1052,7 @@
     });
 
     const mapBoxKey =
-      // "pk.eyJ1IjoiY2VyaXZpdG9zIiwiYSI6ImNqeGc2OG5hcTBtemIzb3BqM3AwZHR6N2IifQ.Fq_N-T_CxLtdw47NprlN_g";
-      "pk.eyJ1IjoiY2VyaXZpdG9zIiwiYSI6ImNqeDIxa2pzYzAyOGg0NXIxeHJldmQxODcifQ.MGTWAtdpMBak95AV4gsY4w";
+      "pk.eyJ1IjoiY2VyaXZpdG9zIiwiYSI6ImNqeGc2OG5hcTBtemIzb3BqM3AwZHR6N2IifQ.Fq_N-T_CxLtdw47NprlN_g";
 
     //From https://www.geodatasource.com/developers/javascript
     function calculateDistance(lat1, lon1, lat2, lon2, unit) {
--- src/keys.js
+++ src/keys.js
@@ -1,3 +1,2 @@
 export const mapBoxKey =
-  // "pk.eyJ1IjoiY2VyaXZpdG9zIiwiYSI6ImNqeGc2OG5hcTBtemIzb3BqM3AwZHR6N2IifQ.Fq_N-T_CxLtdw47NprlN_g";
-  "pk.eyJ1IjoiY2VyaXZpdG9zIiwiYSI6ImNqeDIxa2pzYzAyOGg0NXIxeHJldmQxODcifQ.MGTWAtdpMBak95AV4gsY4w";
+  "pk.eyJ1IjoiY2VyaXZpdG9zIiwiYSI6ImNqeGc2OG5hcTBtemIzb3BqM3AwZHR6N2IifQ.Fq_N-T_CxLtdw47NprlN_g";
